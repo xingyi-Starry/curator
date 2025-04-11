d4ed48a9b463bf2c7c49ca07e0e5d0c36701db29
Trung Vu <vu.trung.96@gmail.com>
2025-04-11 23:00:00 +0000

    ref: remove curator viewer related code

--- publish_pkg.sh
+++ publish_pkg.sh
@@ -3,15 +3,8 @@
 # Install 
 make install
 
-# Build Curator Viewer
-python build_pkg.py
-
 # Build Curator Package
 poetry build
 
 # Publish Curator Package
 poetry publish
-
-# Clean up
-rm -rf src/bespokelabs/curator/viewer/static
-rm -rf dist
